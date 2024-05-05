338e29d63c47f8da7dcf3e4019bee269a0f4442e
Androff <rageclock23@gmail.com>
2024-05-05 12:26:55 +0200

    testmode

--- script.js
+++ script.js
@@ -18,6 +18,16 @@ info.addEventListener("click", openCloseModal);
 
 close.addEventListener("click", openCloseModal);
 
+main.addEventListener("input", (e) => {
+  if (e.target && e.target.classList.contains("contribution")) {
+    if (e.target.value.match(/\D/g)) {
+      e.target.classList.add("red");
+    } else {
+      e.target.classList.remove("red");
+    }
+  }
+});
+
 modal.addEventListener("click", (e) => {
   if (e.target === modal) {
     openCloseModal();
@@ -35,7 +45,7 @@ function createCard() {
   <div>
   <label for="bbb">Имя</label>
   <input class="name" type="text" placeholder="Введите имя" />
-  <label for="bbb">Заплатил</label>
+  <label for="bbb">Портаченная сумма</label>
   <input class="contribution" type="text" placeholder="Внесенная сумма" />
 </div>
   `;
@@ -65,7 +75,19 @@ function getStat() {
 
 function calculate() {
   resultbtn.innerText = "";
-  console.log(contributionArr.length);
+  let result = "";
+  let index;
+  if (
+    contributionArr.some((el, i) => {
+      index = i;
+      return isNaN(el);
+    })
+  ) {
+    resultbtn.innerText = `Проверьте внесенные данные для ${namesArr[index]} - в поле 'Потраченная сумма' должно быть число!`;
+    namesArr.length = 0;
+    contributionArr.length = 0;
+    return;
+  }
   let sum =
     contributionArr.reduce((acc, values) => {
       return acc + values;
@@ -74,45 +96,40 @@ function calculate() {
   let owerpayers = [],
     lowerpayers = [];
 
-  console.log(sum);
-  let result = "";
-
   for (let i = 0; i < namesArr.length; i++) {
     result += `${namesArr[i]} заплатил ${contributionArr[i]} \n`;
-    console.log(`${namesArr[i]} заплатил ${contributionArr[i]}`);
     if (contributionArr[i] >= sum) {
       owerpayers.push([namesArr[i], contributionArr[i] - sum]);
     } else {
       lowerpayers.push([namesArr[i], sum - contributionArr[i]]);
     }
   }
-
-  console.log(owerpayers, owerpayers.length);
-  console.log(lowerpayers);
+  result += ` \n`;
 
   for (let i = 0; i < owerpayers.length; i++) {
     for (let j = 0; j < lowerpayers.length; j++) {
       if (owerpayers[i][1] == 0 || lowerpayers[j][1] == 0) {
         continue;
       } else if (owerpayers[i][1] >= lowerpayers[j][1]) {
-        result += `${lowerpayers[j][0]} платит ${owerpayers[i][0]} сумму в ${lowerpayers[j][1]} денег \n `;
-
-        console.log(`${lowerpayers[j][0]} рассчитался! \n`);
+        result += `${lowerpayers[j][0]} платит ${
+          owerpayers[i][0]
+        } сумму в ${lowerpayers[j][1].toFixed(2)}\n `;
 
         owerpayers[i][1] -= lowerpayers[j][1];
         lowerpayers[j][1] = 0;
       }
       if (owerpayers[i][1] < lowerpayers[j][1]) {
-        result += `${lowerpayers[j][0]} платит ${owerpayers[i][0]} сумму в ${owerpayers[i][1]} денег \n`;
-        console.log(`${owerpayers[i][0]} получил свои деньги! \n`);
+        result += `${lowerpayers[j][0]} платит ${
+          owerpayers[i][0]
+        } сумму в ${owerpayers[i][1].toFixed(2)}\n`;
 
         lowerpayers[j][1] -= owerpayers[i][1];
         owerpayers[i][1] = 0;
       }
     }
   }
+
   resultbtn.innerText = result;
   namesArr.length = 0;
   contributionArr.length = 0;
-  console.log(result);
 }
--- test.js
+++ test.js
@@ -1,31 +1,16 @@
-const customers = [10, 4, 6, 3, 6],
-  n = 5;
+let arr = [1, 5, NaN, 5];
 
-function queueTime(customers, n) {
-  if (customers.length == 0) {
-    return 0;
-  } else if (customers.length < n) {
-    return Math.max(...customers);
+function terst(arr) {
+  if (
+    arr.some((el) => {
+      console.log(el);
+      return isNaN(el);
+    })
+  ) {
+    console.log("yes");
+    return;
   }
-  let kassy = [];
-  for (let i = 0; i < n; i++) {
-    kassy.push(customers.shift());
-  }
-  while (customers.length > 0) {
-    kassy.sort(function (a, b) {
-      return a - b;
-    });
-
-    kassy[0] += customers.shift();
-  }
-  console.log(kassy, customers);
-  return Math.max(...kassy);
+  console.log("false");
 }
 
-console.log(queueTime(customers, n));
-
-console.log(Array(5).fill(0));
-
-console.log(-1 + 0);
-
-Array(1).fill(0);
+terst(arr);
